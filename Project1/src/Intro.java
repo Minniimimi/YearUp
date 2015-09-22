1cfc5c19006a2dd395c44c2fc99943f3291ad56f
Minniimimi <Apark@atl.yearup.org>
2015-09-22 11:21:40 -0400

    coolios

---
+++ Project1/src/Intro.java
@@ -0,0 +1,15 @@
+
+public class Intro {
+
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println("my name is Amy.");
+		System.out.println("My favorite movie is Inception.");
+		System.out.println("I read Harry Potter this month.");
+	}
+	
+	
+}
+
